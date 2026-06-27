5403ad2203a6a34d675c176b61aa6847cdc452a6
Laxmikant <akshayshendurkar55@gmail.com>
2026-06-27 15:54:20 +0530

    Added custom HTML website hosted on AWS EC2 using Nginx

--- main.tf
+++ main.tf
@@ -10,9 +10,37 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+
+resource "aws_security_group" "demo_sg" {
+  name        = "terraform-demo-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "demo_server" {
-  ami           = "ami-0f918f7e67a3323f0"
-  instance_type = var.instance_type
+  ami                    = "ami-0f918f7e67a3323f0"
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
 
   tags = {
     Name = var.instance_name
